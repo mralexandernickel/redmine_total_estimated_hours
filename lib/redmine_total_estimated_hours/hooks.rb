8ff88659f64740ae3d901d7eb196e748104355e0
Alexander Nickel <mr.alexander.nickel@gmail.com>
2013-01-25 17:16:14 +0100

    cleaned the code a little...

--- lib/redmine_total_estimated_hours/hooks.rb
+++ lib/redmine_total_estimated_hours/hooks.rb
@@ -2,7 +2,7 @@ module RedmineTotalEstimatedHours
   class Hooks < Redmine::Hook::ViewListener
     def view_projects_show_sidebar_bottom(context={})
       project = context[:project]
-      cond = project.project_condition(Setting.display_subprojects_issues?)
+      cond = project.project_condition Setting.display_subprojects_issues?
       estimated_hours = Issue.visible.sum :estimated_hours, :include => :project, :conditions => cond
       return "<h3>Geplante Zeit</h3><p><span class=\"icon icon-time\">#{l_hours(estimated_hours)}</span></p>"
     end
