bf1ade0d9c49d2ff11bb2823a65c837afadec17d
Alexander Nickel <mr.alexander.nickel@gmail.com>
2013-01-25 16:32:40 +0100

    added the condition from the projects_controller... i dont think that this is working

--- lib/redmine_total_estimated_hours/hooks.rb
+++ lib/redmine_total_estimated_hours/hooks.rb
@@ -1,8 +1,9 @@
 module RedmineTotalEstimatedHours
   class Hooks < Redmine::Hook::ViewListener
     def view_projects_show_sidebar_bottom(context={})
-      @estimated_hours = Issue.visible.sum :estimated_hours, :include => :project
-      return "<h3>Geplante Zeit</h3><p><span class=\"icon icon-time\">#{l_hours(@estimated_hours)}</span></p>"
+      cond = @project.project_condition(Setting.display_subprojects_issues?)
+      estimated_hours = Issue.visible.sum :estimated_hours, :include => :project, :cond = cond
+      return "<h3>Geplante Zeit</h3><p><span class=\"icon icon-time\">#{l_hours(estimated_hours)}</span></p>"
     end
   end
 end
