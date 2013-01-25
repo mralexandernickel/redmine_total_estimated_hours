30b2c8cc85d036375c8fc3cfbc66bec0049925ca
Alexander Nickel <mr.alexander.nickel@gmail.com>
2013-01-25 17:27:21 +0100

    Added Localization

--- lib/redmine_total_estimated_hours/hooks.rb
+++ lib/redmine_total_estimated_hours/hooks.rb
@@ -4,7 +4,7 @@ module RedmineTotalEstimatedHours
       project = context[:project]
       cond = project.project_condition Setting.display_subprojects_issues?
       estimated_hours = Issue.visible.sum :estimated_hours, :include => :project, :conditions => cond
-      return "<h3>Geplante Zeit</h3><p><span class=\"icon icon-time\">#{l_hours(estimated_hours)}</span></p>"
+      return "<h3>#{l(:field_estimated_hours)}</h3><p><span class=\"icon icon-time\">#{l_hours(estimated_hours)}</span></p>"
     end
   end
 end
