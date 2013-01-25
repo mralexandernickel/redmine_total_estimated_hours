a8e339869d65c2e4f9bfac49ed323e3225e429a6
Alexander Nickel <mr.alexander.nickel@gmail.com>
2013-01-25 17:30:30 +0100

    added urls to my github-page...

--- init.rb
+++ init.rb
@@ -5,7 +5,7 @@ Redmine::Plugin.register :redmine_total_estimated_hours do
   name 'Redmine Total Estimated Hours'
   author 'Alexander Nickel'
   description 'This is a plugin to show the Total Estimated Hours in the Projects Overview Sidebar'
-  version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  version '0.1.0'
+  url 'http://mralexandernickel.github.com'
+  author_url 'http://mralexandernickel.github.com'
 end
